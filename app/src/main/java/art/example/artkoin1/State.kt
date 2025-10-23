d5fa6b39d1e6230f550cd8141a1160d7f1677ced
artur <artur.arciuch@gmail.com>
2025-10-23 12:43:17 +0200

    poprawka var

--- app/src/main/java/art/example/artkoin1/State.kt
+++ app/src/main/java/art/example/artkoin1/State.kt
@@ -1,38 +1,28 @@
 package art.example.artkoin1
 
-import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.application
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
 import org.koin.androidx.compose.koinViewModel
@@ -76,7 +66,7 @@ fun State(navController: NavController, vm: StateVM = koinViewModel()) {
                 Spacer(modifier = Modifier.width(8.dp))
                 Text("Czy zapisywać")
             }
-            Button(onClick = { vm.setZmienna1("zminna1_B", save = shouldSave)}) {
+            Button(onClick = { vm.setZmienna1("Ala ma kota", save = shouldSave)}) {
                 Text("Zmień wartość")
             }
         }
